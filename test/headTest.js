5503e70b4a0ac363339adba3795070e9a7c0f30d
sudhasarathi <sudhav158@email.com>
2023-01-16 21:48:01 -0700

    mocha commit

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,13 @@
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
-// module.exports = assertEqual;
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  describe("#head", () => {
+    it("returns '5' for ['5']", () => {
+      assert.strictEqual(head(['8']), '5');
+    });
+  });
+});
